21ca50b15095a4cf7d7144d7006a9fa60e1d5886
ia-pixel <ytitikpina98@gmail.com>
2025-01-23 14:57:15 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RCW
+ Cours rcw hiver 2025 groupe 1001
---
+++ tools/app.c
@@ -0,0 +1,9 @@
+#include  stdio.h>
+
+int i,y;
+
+for (i=0;i<=10;i++){
+    for (y=0;y<=10;y++){
+        printf("%d x %d = %d\n", i, y, i*y);    
+    }
+}
